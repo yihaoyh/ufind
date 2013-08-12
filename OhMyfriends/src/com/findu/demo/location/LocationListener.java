f46a04b53f5be39058cd0981d3dde1ed3adf4f10
yihao <yhchinabest@gmail.com>
2013-08-12 22:07:20 +0800

    clean some useless code

--- OhMyfriends/src/com/findu/demo/location/LocationListener.java
+++
@@ -1,9 +0,0 @@
-package com.findu.demo.location;
-
-import com.baidu.location.BDLocation;
-
-public interface LocationListener {
-
-	public void onReceiveLocation(BDLocation location);
-	public void onReceivePoi(BDLocation poiLocation);
-}
